b30463131075dd80f586850b465c931ab7ac17b6
Tasnimsomo <tasnasomo@gmail.com>
2024-02-28 01:22:44 -0800

    0x1D. C - Binary trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - Calculates the size of a binary tree.
+ * @tree: A pointer to the root node of the tree.
  *
+ * This function recursively calculates the size of the binary tree rooted
+ * at the given node. The size of a binary tree is defined as the total
+ * number of nodes present in the tree.
  *
+ * Return: The size of the binary tree. If the tree is NULL, returns 0.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
